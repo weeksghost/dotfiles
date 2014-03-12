0d060cd850d0fa92ced8a54f50d686ade57e7191
Erik Marty <emarty@broadway.com>
2014-03-12 00:27:40 +0000

    Latest dotfiles

--- ubuntu-conf.sh
+++ ubuntu-conf.sh
@@ -2,7 +2,6 @@
 
 #symlink ubuntu config files
 ln -s $HOME/dotfiles/ubuntu/.vim .vim
-ln -s $HOME/dotfiles/ubuntu/.bashrc .bashrc
 ln -s $HOME/dotfiles/ubuntu/.bash_aliases .bash_aliases
 ln -s $HOME/dotfiles/ubuntu/.vimrc .vimrc
 ln -s $HOME/dotfiles/ubuntu/.vimrc.local .vimrc.local
