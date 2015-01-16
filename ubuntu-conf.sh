4c108e7d48c7720a8881820aa46029c4ab182faf
Erik Marty <erikomarty@gmail.com>
2015-01-16 15:58:47 -0500

    Updated to port in actual user

--- ubuntu-conf.sh
+++ ubuntu-conf.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #symlink ubuntu config files
-ln -s $HOME/dotfiles/ubuntu/.vim .vim
-ln -s $HOME/dotfiles/ubuntu/.bash_aliases .bash_aliases
-ln -s $HOME/dotfiles/ubuntu/.vimrc .vimrc
-ln -s $HOME/dotfiles/ubuntu/.vimrc.local .vimrc.local
-ln -s $HOME/dotfiles/ubuntu/.gitconfig .gitconfig
+ln -s /home/$USER/dotfiles/ubuntu/.vim .vim
+ln -s /home/$USER/dotfiles/ubuntu/.bash_aliases .bash_aliases
+ln -s /home/$USER/dotfiles/ubuntu/.vimrc .vimrc
+ln -s /home/$USER/dotfiles/ubuntu/.vimrc.local .vimrc.local
+ln -s /home/$USER/dotfiles/ubuntu/.gitconfig .gitconfig
